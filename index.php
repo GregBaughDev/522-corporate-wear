06e56bac1c927ae13dfdb027f4aded488322facd
Greg <80447308+GregBDesign@users.noreply.github.com>
2022-01-16 11:49:53 +1100

    Adds home page content and styles

--- index.php
+++ index.php
@@ -1,11 +1,29 @@
 <?php
     include("./view/templates/header.php");
     include("./view/templates/sidebar.php");
-?>
+    ?>
+
+<link rel="stylesheet" href="./public/styles/index.css">
+
             <div class="main-body">
                 <header>
                     <h1>CorporateWear</h1>
                 </header>
+                <section>
+                    <div class="main-img">
+                        <img src="./public/assets/images/Home/home2.jpg" alt="">
+                        <img src="./public/assets/images/Home/home1.jpg" alt="">
+                    </div>
+                    <h2>A new standard in bespoke work clothing</h2>
+                    <p>
+                        Welcome to CorporateWear. We are the premier suppliers of bespoke uniforms and merchandise in Australia. 
+                        We provide high quality, durable and sleek uniforms that are fully customisable to your business needs. 
+                        Our friendly client liaison team are available to assist with all your needs, requirements and queries. <a href="./view/contact.php">Contact us today!</a> 
+                    </p>
+                    <p>
+                    Our current clients include Crown Casino, Treasury Casino and Hotel, The Star Entertainment group and Movie World Resort.
+                    </p>
+                </section>
             </div>
         </main>
     </body>
