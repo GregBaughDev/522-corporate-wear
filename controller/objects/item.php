5c17740a5468c5950dbd5c7b9a8db337074a359d
Greg <80447308+GregBDesign@users.noreply.github.com>
2022-01-19 20:26:08 +1100

    Adds initial objects and db populate script

---
+++ controller/objects/item.php
@@ -0,0 +1,27 @@
+<?php
+
+class Item {
+    private $colour;
+    private $name;
+    private $category;
+    private $size;
+    private $gender;
+
+    function __construct($itemColour, $itemName, $itemCategory, $itemSize, $itemGender = "Unisex") {
+        $this->colour = $itemColour;
+        $this->name = $itemName;
+        $this->category = $itemCategory;
+        $this->size = $itemSize;
+        $this->gender = $itemGender;
+    }
+
+    function editItem(string $attribute, string $newValue) {
+        $this->$attribute = $newValue;
+    }
+
+    function getItem(){
+        return $this->name;
+    }
+}
+
+?>
---
+++ db/populatedb.php
@@ -0,0 +1,27 @@
+<?php
+
+$clothing = (
+    'Polo', 
+    'Button Up', 
+    'T Shirt', 
+    'Trousers', 
+    'Shorts', 
+    'Skirt', 
+    'Dress', 
+    'Suit Jacket', 
+    'Work Shorts', 
+    'Boots', 
+    'Shoes', 
+    'Waistcoat',
+    'Coat',
+    'Rainjacket',
+    'Socks',
+    'Handkerchief',
+    'Blouse')
+
+
+// Create 50 items (25 in each category)
+
+// Loop over and add them to the DB
+
+?>
