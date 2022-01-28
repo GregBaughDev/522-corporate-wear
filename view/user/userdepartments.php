efbad7f1b257b8d77606d169fdc3bf72e18bfd19
Greg <80447308+GregBDesign@users.noreply.github.com>
2022-01-28 18:28:35 +1100

    Updates departments functionality

--- view/user/userdepartments.php
+++ view/user/userdepartments.php
@@ -18,8 +18,39 @@
                 <div>
                     <h2>My Departments</h2>
                 </div>
-                <section>
-                    
+                <section class="depts">
+                    <table>
+                        <thead>
+                            <tr>
+                                <td><h3>Department</h3></td>
+                                <td><h3>Edit</h3></td>
+                                <td><h3>Delete</h3></td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach($departments as $department) { ?>
+                                <tr>
+                                    <td><?= ucfirst($department['name']) ?></td>
+                                    <td>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#064663">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                                        </svg>
+                                    </td>
+                                    <td>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#064663">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M6 18L18 6M6 6l12 12" />
+                                        </svg>
+                                    </td>
+                                </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                    <div class="add-dept">
+                        <h4>Add department</h4>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#064663">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                    </div>
                 </section>
             </div>
         </main>
